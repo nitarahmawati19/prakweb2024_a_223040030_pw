bb672f3422337c6a1b24600f58df82f415b44c5a
nitarahmawati19 <nita.223040030.mail.unpas.ac.id>
2024-11-02 14:10:00 +0700

    Video 3 - Property dan Method

---
+++ OOP/produk.php
@@ -0,0 +1,38 @@
+<?php 
+
+class Produk {
+    public $judul = "judul", 
+           $penulis = "penulis",
+           $penerbit = "penerbit",
+           $harga = 0;
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+}
+
+// $produk1 = new Produk();
+// $produk1->judul = "xhunter";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "SAO";
+$produk4->penulis = "Matsuyama";
+$produk4->penerbit = "Sony";
+$produk4->harga = 1000000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Anime : " . $produk4->getLabel();
