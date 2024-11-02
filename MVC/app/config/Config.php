62afde7c87332587382a4f4ead8f11bee41a05f3
nitarahmawati19 <nita.223040030.mail.unpas.ac.id>
2024-11-02 17:48:27 +0700

    MVC Video 1-13

---
+++ MVC/app/config/Config.php
@@ -0,0 +1,10 @@
+
+<?php
+
+define('BASEURL', 'http://localhost/prakweb2024_a_223040030_pw/MVC/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2024_a_223040030_pw');
---
+++ MVC/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php 
+
+class User_model
+{
+    private $nama = 'Nita Rahmawati';
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
+?>
